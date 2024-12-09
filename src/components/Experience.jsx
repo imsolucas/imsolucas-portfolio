4c8786348a769c22f8a70c3bb472cb36b3e85622
Lucas <denglucasyijiN@gmail.com>
2024-12-09 17:52:16 +0800

    portfolio d-3 done

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,43 +1,104 @@
-import { Environment, OrbitControls, Sky, ContactShadows } from "@react-three/drei";
+import {
+  Environment,
+  OrbitControls,
+  Sky,
+  ContactShadows,
+  MeshDistortMaterial,
+  Float,
+  MeshWobbleMaterial,
+} from "@react-three/drei";
 import { Avatar } from "./Avatar";
 import { useControls } from "leva";
 import { Office } from "./Office";
+import { motion } from "framer-motion-3d";
+import { useFrame, useThree } from "@react-three/fiber";
+import { animate, useMotionValue } from "framer-motion";
+import { useEffect } from "react";
+import { framerMotionConfig } from "../config";
 
-export const Experience = () => {
-  const { animation } = useControls({
-    animation: {
-      value: "Typing",
-      options: ["Typing", "Falling", "Standing"],
-    },
-  });
+export const Experience = (props) => {
+  const { section, menuOpened } = props;
+  const {viewport} = useThree();
+
+  const cameraPositionX= useMotionValue();
+  const cameraLookAtX = useMotionValue();
+
+  useEffect(() => {
+	animate(cameraPositionX, menuOpened ? -5 : 0, {
+		...framerMotionConfig	
+	});
+	animate(cameraLookAtX, menuOpened ? 5 : 0, {
+		...framerMotionConfig
+	});
+  }, [menuOpened]);
+
+  useFrame((state) => {
+	state.camera.position.x = cameraPositionX.get();
+	state.camera.lookAt(cameraLookAtX.get(), 0, 0);
+  })
+
   return (
     <>
-      {/* <OrbitControls /> */}
-	  <ambientLight intensity={1} />
-      {/* <Sky />
-      <Environment preset="sunset" />
-      <group position-y={-1}>
-        <ContactShadows
-          opacity={0.42}
-          scale={10}
-          blur={1}
-          far={10}
-          resolution={256}
-          color="#000000"
-        />
-        <Avatar animation={animation} />
-        {animation === "Typing" && (
-          <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
-            <boxGeometry />
-            <meshStandardMaterial color="white" />
+      <ambientLight intensity={1} />
+      <motion.group
+        position={[1.5, 2, 3]}
+        scale={[0.9, 0.9, 0.9]}
+        rotation-y={-Math.PI / 4}
+        animate={{
+          y: section === 0 ? 0 : -1,
+        }}
+      >
+        <Office section={section} />
+      </motion.group>
+
+      {/* SKILLS */}
+      <motion.group position={[1, -3, -15]}
+	   animate={{
+		z: section === 1 ? 0 : -10,
+		y: section === 1 ? -viewport.height : -1.5,
+	   }}
+	  >
+        <directionalLight position={[-5, 3, 5]} intensity={0.5} />
+        <Float>
+          <mesh position={[1, -3, -15]} scale={[2, 2, 2]}>
+            <sphereGeometry />
+            <MeshDistortMaterial
+              opacity={0.8}
+              transparent
+              distort={0.4}
+              speed={4}
+              color={"red"}
+            />
           </mesh>
-        )}
-        <mesh scale={5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
-          <planeGeometry />
-          <meshStandardMaterial color="white" />
-        </mesh>
-      </group> */}
-	  <Office />
+        </Float>
+        <Float>
+          <mesh scaled={[3, 3, 3]} position={[3, 1, -18]}>
+            <sphereGeometry />
+            <MeshDistortMaterial
+              opacity={0.8}
+              transparent
+              distort={0.4}
+              speed={5}
+              color={"yellow"}
+            />
+          </mesh>
+        </Float>
+        <Float>
+          <mesh scaled={[1.4, 1.4, 1.4]} position={[-3, -1, -11]}>
+            <boxGeometry />
+            <MeshWobbleMaterial
+              opacity={0.8}
+              transparent
+              factor={1}
+              speed={5}
+              color={"blue"}
+            />
+          </mesh>
+        </Float>
+        <group scale={[2, 2, 2]} position-y={-1.5}>
+          <Avatar animation={section === 0 ? "Falling" : "Standing"} />
+        </group>
+      </motion.group>
     </>
   );
 };
--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,12 +1,26 @@
+import { motion } from "framer-motion";
+
 const Section = (props) => {
   const { children } = props;
 
   return (
-    <section
+    <motion.section
       className={`h-screen w-screen p-8 max-w-screen-2xl mx-auto flex flex-col items-start justify-center`}
+      initial={{
+        opacity: 0,
+        y: 50,
+      }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+        transition: {
+          duration: 1,
+          delay: 0.6,
+        },
+      }}
     >
       {children}
-    </section>
+    </motion.section>
   );
 };
 
@@ -15,9 +29,9 @@ export const Interface = () => {
     <div className="flex flex-col items-center w-screen">
       <AboutSection />
       <SkillsSection />
-      {/* <Section>
+      <Section>
         <h1>Projects</h1>
-      </Section> */}
+      </Section>
       <ContactSection />
     </div>
   );
@@ -31,17 +45,47 @@ const AboutSection = () => {
         <br />
         <span className="bg-white px-1 italic">Deng Yi Jin, Lucas</span>
       </h1>
-      <p className="text-lg text-gray-600 mt-4">
-        I was a full-stack developer at ELVTD
+      <motion.p
+        className="text-lg text-gray-600 mt-4"
+        initial={{
+          opacity: 0,
+          y: 25,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+          delay: 1.5,
+        }}
+      >
+        I used to work as a full-stack developer at ELVTD
         <br />
-        and a student of 42 Kuala Lumpur.
-      </p>
-      <button
+        and currently still a student of 42 Kuala Lumpur.
+        <br />
+        I'm passionate on bringing the next young generation
+        <br />
+        to the world of technology.
+      </motion.p>
+      <motion.button
         className={`bg-indigo-600 text-white py-4 px-8
 	  rounded-lg font-bold text-lg mt-16`}
+        initial={{
+          opacity: 0,
+          y: 25,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+          delay: 2,
+        }}
       >
         Contact me
-      </button>
+      </motion.button>
     </Section>
   );
 };
@@ -70,15 +114,19 @@ const skills = [
 ];
 const languages = [
   {
-    title: "🇫🇷 French",
+    title: "English",
     level: 100,
   },
   {
-    title: "🇺🇸 English",
-    level: 80,
+    title: "Chinese",
+    level: 60,
   },
   {
-    title: "🇯🇵 Japanese",
+    title: "Malay",
+    level: 40,
+  },
+  {
+    title: "Japanese",
     level: 20,
   },
 ];
@@ -86,83 +134,143 @@ const languages = [
 const SkillsSection = () => {
   return (
     <Section>
-      <h2 className="text-4xl font-bold">Skills</h2>
-      <div className="mt -8 space-y4">
-        {skills.map((skill, index) => (
-          <div className="w-64" key={index}>
-            <h3 className="text-xl font-bold text-gray800">{skill.title}</h3>
-            <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
-              <div
-                className="h-full bg-indigo-500 rounded-full"
-                style={{ width: `${skill.level}%` }}
-              />
-            </div>
-          </div>
-        ))}
-      </div>
-      <div>
-        <h2 className="text-4xl font-bold mt-10">Languages</h2>
-        <div className="mt-8 space-y-4">
-          {languages.map((lng, index) => (
+      <motion.div whileInView={"visible"}>
+        <h2 className="text-4xl font-bold">Skills</h2>
+        <div className="mt -8 space-y4">
+          {skills.map((skill, index) => (
             <div className="w-64" key={index}>
-              <h3 className="text-xl font-bold text-gray-800">{lng.title}</h3>
+              <motion.h3
+                className="text-xl font-bold text-gray800"
+                initial={{
+                  opacity: 0,
+                }}
+                variants={{
+                  visible: {
+                    opacity: 1,
+                    transition: {
+                      duration: 1,
+                      delay: 1 + index * 0.2,
+                    },
+                  },
+                }}
+              >
+                {skill.title}
+              </motion.h3>
               <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
-                <div
+                <motion.div
                   className="h-full bg-indigo-500 rounded-full"
-                  style={{ width: `${lng.level}%` }}
+                  style={{ width: `${skill.level}%` }}
+                  initial={{
+                    scaleX: 0,
+                    originX: 0,
+                  }}
+                  variants={{
+                    visible: {
+                      scaleX: 1,
+                      transition: {
+                        duration: 1,
+                        delay: 1 + index * 0.2,
+                      },
+                    },
+                  }}
                 />
               </div>
             </div>
           ))}
         </div>
-      </div>
+        <div>
+          <h2 className="text-4xl font-bold mt-10">Languages</h2>
+          <div className="mt-8 space-y-4">
+            {languages.map((lng, index) => (
+              <div className="w-64" key={index}>
+              <motion.h3
+                className="text-xl font-bold text-gray800"
+                initial={{
+                  opacity: 0,
+                }}
+                variants={{
+                  visible: {
+                    opacity: 1,
+                    transition: {
+                      duration: 1,
+                      delay: 2 + index * 0.2,
+                    },
+                  },
+                }}
+              >
+                {lng.title}
+              </motion.h3>
+              <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
+                <motion.div
+                  className="h-full bg-indigo-500 rounded-full"
+                  style={{ width: `${lng.level}%` }}
+                  initial={{
+                    scaleX: 0,
+                    originX: 0,
+                  }}
+                  variants={{
+                    visible: {
+                      scaleX: 1,
+                      transition: {
+                        duration: 1,
+                        delay: 2 + index * 0.2,
+                      },
+                    },
+                  }}
+                />
+              </div>
+            </div>
+            ))}
+          </div>
+        </div>
+      </motion.div>
     </Section>
   );
 };
 
 const ContactSection = () => {
-	return (
-	  <Section>
-		<h2 className="text-5xl font-bold">Contact me</h2>
-		<div className="mt-8 p-8 rounded-md bg-white w-96 max-w-full">
-		  <form>
-			<label for="name" className="font-medium text-gray-900 block mb-1">
-			  Name
-			</label>
-			<input
-			  type="text"
-			  name="name"
-			  id="name"
-			  className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-			/>
-			<label
-			  for="email"
-			  className="font-medium text-gray-900 block mb-1 mt-8"
-			>
-			  Email
-			</label>
-			<input
-			  type="email"
-			  name="email"
-			  id="email"
-			  className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-			/>
-			<label
-			  for="email"
-			  className="font-medium text-gray-900 block mb-1 mt-8"
-			>
-			  Message
-			</label>
-			<textarea
-			  name="message"
-			  id="message"
-			  className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-			/>
-			<button className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 ">
-			  Submit
-			</button>
-		  </form>
-		</div>
-	  </Section>
-	);
-  };
+  return (
+    <Section>
+      <h2 className="text-5xl font-bold">Contact me</h2>
+      <div className="mt-8 p-8 rounded-md bg-white w-96 max-w-full">
+        <form>
+          <label for="name" className="font-medium text-gray-900 block mb-1">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+          />
+          <label
+            for="email"
+            className="font-medium text-gray-900 block mb-1 mt-8"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+          />
+          <label
+            for="email"
+            className="font-medium text-gray-900 block mb-1 mt-8"
+          >
+            Message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+          />
+          <button className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 ">
+            Submit
+          </button>
+        </form>
+      </div>
+    </Section>
+  );
+};
